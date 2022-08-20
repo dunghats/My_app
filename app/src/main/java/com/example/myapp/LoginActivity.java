dd820e91cf00f622538a4a2e51ecc3a8afeeb618
Hoàng Dũng <hoanganhdung008ts001@gmail.com>
2022-08-20 14:37:56 +0700

    first commit

---
+++ app/src/main/java/com/example/myapp/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+private Button btnlogin;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        btnlogin = findViewById(R.id.btnLogin);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+            }
+        });
+    }
+}
